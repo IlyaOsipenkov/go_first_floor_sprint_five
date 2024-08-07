2b9c5ab29a68e674eeff46fffd7ad5e225ad0b2d
IlyaOsipenkov <dakarlog@gmail.com>
2024-08-07 22:12:27 +0300

    fix walking func value -- fourth iteration

--- ftracker.go
+++ ftracker.go
@@ -102,8 +102,9 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
-	caloriesOfWalking := ((walkingCaloriesWeightMultiplier*weight + (math.Pow((meanSpeed(action, duration))*kmhInMsec, 2)/height)*walkingSpeedHeightMultiplier*weight) * duration * minInH)
-	return caloriesOfWalking
+	// caloriesOfWalking := ((walkingCaloriesWeightMultiplier*weight + (math.Pow((meanSpeed(action, duration))*kmhInMsec, 2)/height)*walkingSpeedHeightMultiplier*weight) * duration * minInH)
+	// return caloriesOfWalking
+	return ((walkingCaloriesWeightMultiplier*weight + (math.Pow(meanSpeed(action, duration)*kmhInMsec, 2)/height)*walkingSpeedHeightMultiplier*weight) * duration * minInH)
 }
 
 // Константы для расчета калорий, расходуемых при плавании.
